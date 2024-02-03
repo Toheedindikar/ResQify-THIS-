0f8c7c63043ed92e808cad4d3a674788d6729ec7
Pratham-H-S <prathamsunnal10@gmail.com>
2024-02-03 11:15:26 +0530

    working

--- PythonGuides/PythonGuides/settings.py
+++ PythonGuides/PythonGuides/settings.py
@@ -34,7 +34,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-xewhry7ry0*+1^w05%l^!bbtiwitpe80w0-b^w4$^vkc-20&a("
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 GOOGLE_API_KEY = 'AIzaSyAbqSUWRSga4wxQFfaiteZBsi-0hUi87as'
 
 
